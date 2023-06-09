4c04c761d60040839784bbb2165f8df1aa53f5e0
Seif Farag <s.farag@innopolis.university>
2023-06-09 13:59:59 +0300

    impove logic and fix minor spelling mistakes

--- detect_emotion.py
+++ detect_emotion.py
@@ -3,39 +3,41 @@ from deepface import DeepFace
 import numpy as np  # just for proper typing
 
 
-def get_image(url: str) -> tuple[cv2.VideoCapture, tuple[bool, np.ndarray]]:
+def get_image(url: str) -> tuple[bool, np.ndarray]:
     """Get a streamed image from a URL. WARNING! This will NOT work with video streams
 
     Args:
-        url (str): image link to be processed
+        url (str): Image link to be processed
 
     Returns:
-        tuple[cv2.VideoCapture, tuple[bool, np.ndarray]]: returns a VideoCapture object, check variable, and an image
+        tuple[bool, np.ndarray]: Returns a check variable and an image
     """
     cap = cv2.VideoCapture(url)
-    return cap, cap.read()
+    success, img = cap.read()
+    cap.release()
+    return success, img
 
 
 def get_face_area(region: dict[str, int]) -> int:
-    """claculate face area give its x,y,h,w region from DeepFace
+    """Calculate face area give its x,y,h,w region from DeepFace
 
     Args:
-        region (dict[str, int]): a dictionary with x,y,w,h as keys
+        region (dict[str, int]): A dictionary with x,y,w,h as keys
 
     Returns:
-        int: returns face area
+        int: Returns face area
     """
     return region["w"] * region["h"]
 
 
-def emotion_model(img: np.ndarray) -> tuple[bool, str]:
-    """analyze facial emotions with DeepFace for all faces in the image, only biggest face area is considered.
+def run_emotion_model(img: np.ndarray) -> str:
+    """Run the emotion detection backend with DeepFace for all faces in the image. Other backends can be added later on.
 
     Args:
-        img (np.ndarray): input image
+        img (np.ndarray): Input image
 
     Returns:
-        tuple[bool, str]: emotion of the biggest face in the picture
+        str: Emotion of the biggest face in the picture or None if no face was found
     """
     try:
         objs = DeepFace.analyze(
@@ -44,12 +46,12 @@ def emotion_model(img: np.ndarray) -> tuple[bool, str]:
         areas = [get_face_area(obj["region"]) for obj in objs]
         biggest_face_idx = areas.index(max(areas))
         biggest_face_emotion = objs[biggest_face_idx]["dominant_emotion"]
-        return True, biggest_face_emotion
+        return biggest_face_emotion
     except ValueError:
         print(
             "Face could not be detected. Please confirm that the picture is a face photo."
         )
-        return False, None
+        return None
 
 
 def get_emotion(url: str) -> str:
@@ -64,12 +66,9 @@ def get_emotion(url: str) -> str:
     Returns:
         str: Detected emotion of the biggest face in the image
     """
-    cap, (success, img) = get_image(url)
-    cap.release()
+    success, img = get_image(url)
     if success:
-        found, emotion = emotion_model(img)
-        if not found:
-            return None
+        emotion = run_emotion_model(img)
         return emotion
     else:
         raise IOError("URL not valid or not an image", "URL is: {url}")
